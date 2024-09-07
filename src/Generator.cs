a3bbcc87874fb1945276117cebf9404d2315223e
Scott Norton <scotty112@live.com>
2024-09-07 00:35:55 -0400

    fix formatting

--- src/Generator.cs
+++ src/Generator.cs
@@ -1,4 +1,8 @@
-﻿namespace TSExportGenerator {
+﻿// ╔═══════════════════════════════════════════════════════════════════════════════╗
+//    File: Generator.cs - Author: Scott Norton
+// ╚═══════════════════════════════════════════════════════════════════════════════╝
+
+namespace TSExportGenerator {
 	using System;
 	using System.Collections.Generic;
 	using System.IO;
